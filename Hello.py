fa9f376ac731f805063c4e9306d16ed3f1c0e769
Jan Majta <dzanek@users.noreply.github.com>
2024-04-24 16:26:40 +0000

    fix

--- Hello.py
+++ Hello.py
@@ -23,9 +23,32 @@ import flickrapi
 # Flickr API key and secret
 API_KEY = st.secrets["API_KEY"]
 API_SECRET = st.secrets["API_SECRET"]
-# Initialize the Flickr API
+
 flickr = flickrapi.FlickrAPI(API_KEY, API_SECRET,  format='parsed-json')
-flickr.authenticate_via_browser(perms='read')
+
+print('Step 1: authenticate')
+
+# Only do this if we don't have a valid token already
+if not flickr.token_valid(perms='read'):
+
+    # Get a request token
+    flickr.get_request_token(oauth_callback='oob')
+
+    # Open a browser at the authentication URL. Do this however
+    # you want, as long as the user visits that URL.
+    authorize_url = flickr.auth_url(perms='read')
+    webbrowser.open_new_tab(authorize_url)
+
+    # Get the verifier code from the user. Do this however you
+    # want, as long as the user gives the application the code.
+    verifier = str(input('Verifier code: '))
+
+    # Trade the request token for an access token
+    flickr.get_access_token(verifier)
+
+# Initialize the Flickr API
+#flickr = flickrapi.FlickrAPI(API_KEY, API_SECRET,  format='parsed-json')
+#flickr.authenticate_via_browser(perms='read')
 
 # GPS coordinates
 lat = data[0]  # Latitude
