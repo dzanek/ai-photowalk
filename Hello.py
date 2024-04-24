11b03d56057300ba5d6a34d6752dc330050fb752
Jan Majta <dzanek@users.noreply.github.com>
2024-04-24 16:31:51 +0000

    fix

--- Hello.py
+++ Hello.py
@@ -26,25 +26,7 @@ API_SECRET = st.secrets["API_SECRET"]
 
 flickr = flickrapi.FlickrAPI(API_KEY, API_SECRET,  format='parsed-json')
 
-print('Step 1: authenticate')
 
-# Only do this if we don't have a valid token already
-if not flickr.token_valid(perms='read'):
-
-    # Get a request token
-    flickr.get_request_token(oauth_callback='oob')
-
-    # Open a browser at the authentication URL. Do this however
-    # you want, as long as the user visits that URL.
-    authorize_url = flickr.auth_url(perms='read')
-    webbrowser.open_new_tab(authorize_url)
-
-    # Get the verifier code from the user. Do this however you
-    # want, as long as the user gives the application the code.
-    verifier = str(input('Verifier code: '))
-
-    # Trade the request token for an access token
-    flickr.get_access_token(verifier)
 
 # Initialize the Flickr API
 #flickr = flickrapi.FlickrAPI(API_KEY, API_SECRET,  format='parsed-json')
