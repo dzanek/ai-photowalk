ec6e0bd5247653622680808f29dfca3507cfc49d
Jan Majta <dzanek@users.noreply.github.com>
2024-04-25 11:32:59 +0000

    fix

--- Hello.py
+++ Hello.py
@@ -99,6 +99,7 @@ model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 print('Function to extract features from an image')
 def extract_features(img_path):
     img = image.load_img(img_path, target_size=(224, 224), color_mode="grayscale")
+    st.write(img)
     img_data = image.img_to_array(img)
     img_data = np.expand_dims(img_data, axis=0)
     img_data = preprocess_input(img_data)
