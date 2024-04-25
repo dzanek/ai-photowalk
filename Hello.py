a22d7e7548cc05d1dc5d4905870ff836424f5d3f
Jan Majta <dzanek@users.noreply.github.com>
2024-04-25 14:37:13 +0000

    fix

--- Hello.py
+++ Hello.py
@@ -104,7 +104,7 @@ def extract_features(img_path):
     img_data2 = image.img_to_array(img2)
     st.write("data")
     st.write(img_data[0])
-    img_data2 = np.repeat(img_data2[..., np.newaxis], 3, -1)[0]
+    #img_data2 = np.repeat(img_data2[..., np.newaxis], 3, -1)[0]
     st.write(f"pseudergb")
     st.write(img_data2[0])
     img_data = np.expand_dims(img_data, axis=0)
